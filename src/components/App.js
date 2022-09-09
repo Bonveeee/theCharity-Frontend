52937db521076e49a24aa35c95eef9a912d92330
bonveeee <bonochieng@gmail.com>
2022-09-09 05:00:24 +0300

    updates on components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Campaigns from "./Campaigns";
-// import Donations from "./Donations";
-// import Donors from "./Donors";
-// import Home from "./Home";
-// import Organizations from "./Organizations";
-// import ReportsReviews from "./ReportsReviews";
+import Campaigns from "./Campaigns";
+import Donations from "./Donations";
+import Donors from "./Donors";
+import Home from "./Home";
+
 
 //this is main  parent component
 
@@ -17,14 +16,12 @@ function App() {
         <Navbar />
       </div>
 
-      {/* <Routes>
+      <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/Donations" element={<Donations />}></Route>
         <Route exact path="/Donors" element={<Donors />}></Route>
         <Route exact path="/Campaigns" element={<Campaigns />}></Route>
-        <Route exact path="/Organizations" element={<Organizations />}></Route>
-        <Route exact path="/ReportsReviews" element={<ReportsReviews />}></Route>
-      </Routes> */}
+      </Routes>
     </Router>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,19 +12,16 @@ function Navbar() {
                           <Link className="nav-link" to="/">Home</Link>
                       </li>
                       <li className="nav-item">
-                          <Link className="nav-link" to="/ArtWorks">Donations</Link>
+                          <Link className="nav-link" to="/Donations">Donations</Link>
                       </li>
                       <li className="nav-item">
-                          <Link className="nav-link" to="/Artists">Donors</Link>
+                          <Link className="nav-link" to="/Donors">Donors</Link>
                       </li>
                       <li className="nav-item">
-                          <Link className="nav-link" to="/About">Campaigns</Link>
+                          <Link className="nav-link" to="/Campaigns">Campaigns</Link>
                       </li>
                       <li className="nav-item">
-                          <Link className="nav-link" to="/Helpdesk">Organizations</Link>
-                      </li>
-                      <li className="nav-item">
-                          <Link className="nav-link" to="/Helpdesk">Reviews</Link>
+                          <Link className="nav-link" to="/Contacts">Contact Us</Link>
                       </li>
                   </ul>
               </div>
