966c91ba9b32fedde428c2cd2f0650b2533d6418
bonveeee <bonochieng@gmail.com>
2022-09-12 03:09:52 +0300

    update on component campaign

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -23,15 +23,15 @@ function Campaigns() {
   }
 
   const eachCampaign = campaigns.map((campaign) => (
-    <div class="carousel-item" key={campaign.id}>
+    <div className="grid" key={campaign.id}>
      
-      <div class="carousel-caption d-none d-md-block">
+      <div className="cards">
       <img
         src="https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
         alt="kids"
       />
-        <h5>Name: {campaign.name}</h5>
-        <h5>Location: {campaign.location}</h5>
+        <h5>Name:{campaign.name}</h5>
+        <h5>Location:{campaign.location}</h5>
         <h5>Name: {campaign.name}</h5>
         <p>{campaign.description}</p>
         <button onClick={handleClick}>More about the campaign</button>
